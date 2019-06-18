91fd847ed544c738769df13332eb11f6b42a3f5a
enchanting-process-7328 <peterlin5301997@gmail.com>
2019-06-18 20:47:14 +0000

    Automatically backed up by Learn

--- lib/second_challenge.rb
+++ lib/second_challenge.rb
@@ -2,6 +2,10 @@ def second_challenge
   epic_tragedy = {
     :montague => {
       #your key/value pairs here!
+      :patriarch => {name: "Lord Montague", age: "53"},
+      :matriarch => {name: "Lady Montague", age: "54"},
+      :hero => {name: "Romeo", age: "15", status: "alive"},
+      :hero_friends => []
       }, 
     :capulet => {
       #your key/value pairs here!
